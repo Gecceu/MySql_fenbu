14fc3e3ba41593903cdc4b891e78581393f7dfae
isa <3200105318@zju.edu.cn>
2023-05-16 21:28:52 +0800

    负载均衡

--- src/master/Mymaster.java
+++ src/master/Mymaster.java
@@ -11,11 +11,21 @@ import javax.swing.JTextArea;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Mymaster extends JFrame{
     private JTextArea jta = new JTextArea(); //用于提示信息的文本域
     private Container cc;
     private ServerSocket server;
     private Socket socket;
+
+    public static HashMap<String, List<String>> directory = new HashMap<>(); // region_ip : tables
+    
+    public static HashMap<String, String> region_names = new HashMap<>();    // region_id : region_name
+    
+    public static int regions_num = 0;
 
     public Mymaster(){
         super("主节点服务器");
@@ -47,6 +57,34 @@ public class Mymaster extends JFrame{
         }catch(Exception e){
             e.printStackTrace();
         }
+    }
+
+    /*
+    * 通过表名找到ip地址
+     */
+    public String getAddrByTableName(String table_name){
+        for(HashMap.Entry<String, List<String>> entry : directory.entrySet()){
+            if(entry.getValue().contains(table_name)){
+                return new String(entry.getKey());                
+            }
+        }
+        return new String("-1");
+    }
+
+
+    /*
+     * 负载均衡
+     */
+    public String getMostSpareRegion(){
+        int mini = 0;
+        String spare_region_addr = "";
+        for(Map.Entry<String, List<String>> entry : directory.entrySet()){
+            if(entry.getValue().size() < mini){
+                mini = entry.getValue().size();                    
+                spare_region_addr = entry.getKey();
+            }
+        }
+        return spare_region_addr;
     }
 
     private static class Clienthandler implements Runnable{
