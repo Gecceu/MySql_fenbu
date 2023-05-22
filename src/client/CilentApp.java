d498f1b56ed611ea54556b0420e43458d84dbe4f
ROY <113625502+ROY804841943@users.noreply.github.com>
2023-05-22 13:01:52 +0800

    添加客户端线程

--- src/client/CilentApp.java
+++ src/client/CilentApp.java
@@ -1,10 +1,26 @@
 package client;
 
+import java.io.IOException;
+
 import utils.params;
 
 public class CilentApp {
-    public static void main(String[] args){
+    private static final String MASTER_IP = "127.0.0.1";
+    
+    public static void main(String[] args) throws IOException{
         Myclient client = new Myclient();
-        client.connect(params.MatserPort_Client);
+        
+        Thread masterThread = new Thread(new Runnable(){
+            @Override
+            public void run(){
+                try {
+                    client.connect(MASTER_IP,params.MatserPort_Client);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        masterThread.start();
     }
 }
+
